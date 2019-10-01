fdbef69f033e532d5b8ff0b29d8b33eef3391bbe
namgaxilem <namgaxilem@gmail.com>
2019-10-02 00:29:22 +0700

    đv

--- app/app.route.js
+++ app/app.route.js
@@ -108,6 +108,6 @@ rootApp.config(['$stateProvider', '$urlRouterProvider',
         controller: 'loginController'
       });
 
-    $urlRouterProvider.otherwise('/about');
+    $urlRouterProvider.otherwise('/login');
   }
 ]);
